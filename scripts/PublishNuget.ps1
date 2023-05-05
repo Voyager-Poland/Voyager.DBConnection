529dbf8a8632815fadd55d8d88f78beda7c6af05
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2023-05-05 14:04:24 +0200

    literówka

--- scripts/PublishNuget.ps1
+++ scripts/PublishNuget.ps1
@@ -2,7 +2,7 @@ $version='3.0.0'
 dotnet build -c Release   /property:Version=$version
 dotnet pack -c Release /property:Version=$version
 
-$ostatniPakiet = (gci .\src\Voyager.DBConnnection\bin\Release\*.nupkg | select -last 1).Name
-$sciezka = ".\src\Voyager.DBConnnection\bin\Release\$ostatniPakiet"
+$ostatniPakiet = (gci .\src\Voyager.DBConnection\bin\Release\*.nupkg | select -last 1).Name
+$sciezka = ".\src\Voyager.DBConnection\bin\Release\$ostatniPakiet"
 
 dotnet nuget push "$sciezka" -s Voyager-Poland
