9554e1aac333bfa380ed8f0b0f16be489c9eda5b
AndrzejSwistowski <andrzej.swistowski@voyager.pl>
2025-12-13 23:45:22 +0100

    the ParameterValidator in the separated file

--- src/Voyager.DBConnection/Connection.cs
+++ src/Voyager.DBConnection/Connection.cs
@@ -316,26 +316,4 @@ namespace Voyager.DBConnection
 		}
 
 	}
-
-	internal static class ParameterValidator
-	{
-
-		public static void DbGuard(Database db)
-		{
-			if (db == null)
-				throw new NoDbException();
-		}
-
-		public static void DBPolicyGuard(IExceptionPolicy exceptionPolicy)
-		{
-			if (exceptionPolicy == null)
-				throw new LackExceptionPolicyException();
-		}
-
-		public static void DBPolicyGuard(IMapErrorPolicy exceptionPolicy)
-		{
-			if (exceptionPolicy == null)
-				throw new LackExceptionPolicyException();
-		}
-	}
 }
